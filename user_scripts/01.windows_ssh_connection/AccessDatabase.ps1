d2a1ef6bb40ce9f9cc60b8342f5773bdcd96ab39
ltsai323 <johnson20050@gmail.com>
2023-08-18 14:00:47 +0800

    Execute VcXsrv before ssh

--- user_scripts/01.windows_ssh_connection/AccessDatabase.ps1
+++ user_scripts/01.windows_ssh_connection/AccessDatabase.ps1
@@ -2,10 +2,20 @@ $REMOTE_USER = "ntuuser"
 
 
 $env:DISPLAY = "localhost:0"
-$width = 60
-$height = 3
+$width = 80
+$height = 6
 
 $host.UI.RawUI.WindowSize = New-Object System.Management.Automation.Host.Size($width,$height)
 
+
+$X11Program = "C:\Program Files\VcXsrv\xlaunch.exe"
+
+if (Test-Path -Path $X11Program -PathType Leaf) {
+  Start-Process $X11Program
+} else {
+  Write-Host '--------------------------- WARNING ---------------------------------------------'
+  Write-Host '-- Default X11 program not found in computer, you need to execute it manually. --'
+  Write-Host '---------------------------------------------------------------------------------'
+}
 ssh -Y $REMOTE_USER@hep10.phys.ntu.edu.tw 'source gotoworkspace/01.SharedUI.sh && python /data1/ltsai/workspace/git/SharedUI/mainUI.py'
-# Pause
+Pause
